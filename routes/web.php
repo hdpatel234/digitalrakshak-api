98f1417fbcde44dd32480c6c9be70312b7325391
haresh <hdpatel234@gmail.com>
2026-02-21 21:37:55 +0530

    removed

--- routes/web.php
+++ routes/web.php
@@ -1,14 +1,7 @@
 <?php
 
-use Illuminate\Support\Facades\Hash;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
-});
-
-
-Route::get('test', function () {
-    echo Hash::make('User@123');
-    exit();
 });
