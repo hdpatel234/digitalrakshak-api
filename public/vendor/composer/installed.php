d2607e642f6f1e7d950c270a3ce298c2975f9303
haresh <hdpatel234@gmail.com>
2026-07-07 10:21:46 +0530

    chore: install and configure phpseclib library dependencies

---
+++ public/vendor/composer/installed.php
@@ -0,0 +1,32 @@
+<?php return array(
+    'root' => array(
+        'name' => '__root__',
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
+        'reference' => '4f481cd7d63f4c42a8c0b0b511522c692daa2129',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => true,
+    ),
+    'versions' => array(
+        '__root__' => array(
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'reference' => '4f481cd7d63f4c42a8c0b0b511522c692daa2129',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+        'phpseclib/phpseclib' => array(
+            'pretty_version' => '2.0.55',
+            'version' => '2.0.55.0',
+            'reference' => 'd73c9e019a895be83b18a2ccccfa7e2b0a648743',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../phpseclib/phpseclib',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
