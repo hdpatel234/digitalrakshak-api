60c887f3fb425c03202b3df1c39ce3bbbab3ac1b
haresh <hdpatel234@gmail.com>
2026-07-10 09:53:07 +0530

    feat: implement TransactionController to provide paginated transaction list for admin API

--- app/Http/Controllers/Api/Admin/TransactionController.php
+++ app/Http/Controllers/Api/Admin/TransactionController.php
@@ -19,7 +19,24 @@ class TransactionController extends BaseController
         $query = $this->paymentTransactionService->query()
             ->with(['client', 'order', 'invoice', 'gatewayConfig', 'methodType']);
 
-        $datatable = $this->paymentTransactionService->datatable($query, $request->all());
+        $table = $this->paymentTransactionService->query()->getModel()->getTable();
+        $statusColumn = $this->paymentTransactionService->status();
+
+        $params = $request->all();
+        if (isset($params['limit']) && !isset($params['per_page'])) {
+            $params['per_page'] = $params['limit'];
+        }
+
+        $datatable = $this->paymentTransactionService->datatable($query, $params, [
+            'status_column' => $table . '.' . $statusColumn,
+            'date_column' => $table . '.created_at',
+            'allowed_filters' => [
+                'status' => $table . '.' . $statusColumn,
+                'client_id' => $table . '.client_id',
+            ],
+            'default_sort_by' => $table . '.created_at',
+            'default_sort_direction' => 'desc',
+        ]);
 
         return $this->success('Transactions list fetched successfully.', $datatable);
     }
